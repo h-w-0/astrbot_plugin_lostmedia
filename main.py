09ecdf90aa98eb2c2f208816fe773a422cb214de
h-w-0 <2745414403@qq.com>
2026-06-04 14:20:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
 def escape_gql_string(s: str) -> str:
-    
     return '"' + s.replace('\\', '\\\\').replace('"', '\\"') + '"'
 
 @register("astrbot_plugin_lostmedia", "YourName", "失传媒体成员与条目查询", "1.0.0")
